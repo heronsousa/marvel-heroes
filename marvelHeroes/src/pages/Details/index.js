080952e59ba7ed401b8b5d1b812e045362087a97
Heron Rodrigues <heronrs1@gmail.com>
2020-06-13 23:58:01 -0300

    add character info

--- marvelHeroes/src/pages/Details/index.js
+++ marvelHeroes/src/pages/Details/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { ScrollView } from 'react-native';
+import { ScrollView, Image, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-import { Container, CharacterImage } from './styles';
+import { Container, CharacterImage, Character, AlterEgo, TouchableOpacity, Name, Caracteristics, Caracteristic, IconDescription, Icon } from './styles';
 
 import spider from "../../../assets/chars/spider-man.png";
+import back from '../../../assets/icons/back.svg';
+import age from '../../../assets/icons/age.svg';
+import height from '../../../assets/icons/height.svg';
+import weight from '../../../assets/icons/weight.svg';
+import universe from '../../../assets/icons/universe.svg';
 
 export default function Details() {
+    const caracteristics = [
+        {icon: age, value: '30 anos'},
+        {icon: weight, value: '78kg'},
+        {icon: height, value: '1.78m'},
+        {icon: universe, value: 'Terra 616'}
+    ]
 
     return (
         <Container>
@@ -15,6 +26,9 @@ export default function Details() {
                     source={spider}
                     resizeMode='contain'    
                 />
+                <TouchableOpacity>
+                    <Image source={back} tintColor='#fff'/>
+                </TouchableOpacity>
 
                 <LinearGradient
                     colors={['rgba(0,0,0, .20)', 'rgb(0,0,0)']}
@@ -25,6 +39,22 @@ export default function Details() {
                         bottom: 0,
                     }}
                 />
+
+                <Character>
+                    <AlterEgo>Peter Paker</AlterEgo>
+                    <Name>Homem Aranha</Name>
+                </Character>
+
+                <Caracteristics>
+                    {caracteristics.map(caracteristic => {
+                        return(
+                            <Caracteristic key={caracteristic.value}>
+                                <Image source={caracteristic.icon} tintColor='#fff'/>
+                                <IconDescription>{caracteristic.value}</IconDescription>
+                            </Caracteristic>
+                        )
+                    })}
+                </Caracteristics>
             </ScrollView>
         </Container>
     )
--- marvelHeroes/src/pages/Details/styles.js
+++ marvelHeroes/src/pages/Details/styles.js
@@ -8,3 +8,55 @@ export const Container = styled.View`
 export const CharacterImage = styled.Image`
     height: 700px;
 `
+
+export const Name = styled.Text`
+    font-size: 40px;
+    font-weight: bold;
+    color: #fff;
+    max-width: 50%;
+`
+
+export const AlterEgo = styled.Text`
+    font-size: 16px;
+    color: #f8f8f8;
+`
+
+export const Caracteristic = styled.View`
+    align-items: center;
+`
+
+export const IconDescription = styled.Text`
+    font-size: 14px;
+    color: #fff;
+    margin-top: 10px;
+`
+
+export const Character = styled.View`
+    position: absolute;
+    top: 300px; 
+    left: 0; 
+    right: 0;
+    padding: 20px;
+    justify-content: flex-end;
+    align-items: flex-start;
+`
+
+export const TouchableOpacity = styled.TouchableOpacity`
+    position: absolute;
+    top: 20px; 
+    left: 20px; 
+    right: 0;
+    justify-content: flex-end;
+    align-items: flex-start;
+`
+
+export const Caracteristics = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    padding: 20px;
+    top: 460px; 
+    left: 0; 
+    right: 0;
+`
